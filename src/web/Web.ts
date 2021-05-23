6eb1dcf49fa68648ecb94af21e2b1a3ef34889cf
Robin Ferch <robin@ferch-mail.de>
2021-05-23 21:28:42 +0200

    added web core

---
+++ src/web/Web.ts
@@ -0,0 +1,36 @@
+import * as express from 'express';
+import Core from "../Core";
+import Routes from "./routes";
+
+class Web {
+    app;
+    core: Core;
+    routes: Routes;
+    constructor(core: Core) {
+        this.app = express();
+        this.core = core;
+    }
+
+    public startWebserver() {
+        this.app.listen(this.getPort(), () => {
+            this.core.getLogger().info(`Starting webserver on port ${this.getPort()}`)
+            this.routes = new Routes(this);
+        })
+    }
+
+    public getPort() {
+        return process.env.webPort;
+    }
+
+    public getApp() {
+        return this.app;
+    }
+
+    public getCore() {
+        return this.core;
+    }
+
+
+}
+
+export default Web;
