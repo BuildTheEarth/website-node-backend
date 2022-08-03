c3671148de2abf13dfad9ea507b4143fc0e33a34
Robin Ferch <robin@ferch-mail.de>
2022-08-03 16:39:08 +0200

    Switched from log4js to winston for fancier logging

--- src/Core.ts
+++ src/Core.ts
@@ -1,9 +1,9 @@
-import {configure, getLogger, Logger} from 'log4js';
 import Web from './web/Web';
 import * as Keycloak from "keycloak-connect";
 import * as session from "express-session";
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient} from '@prisma/client'
 import KeycloakAdmin from "./util/KeycloakAdmin";
+import * as winston from "winston";
 
 class Core {
     web: Web;
@@ -11,6 +11,7 @@ class Core {
     memoryStore: session.MemoryStore;
     prisma: PrismaClient;
     keycloakAdmin: KeycloakAdmin;
+    logger: winston.Logger;
 
     constructor() {
         this.setUpLogger();
@@ -27,17 +28,41 @@ class Core {
         })
 
 
-
-
-
     }
 
     private setUpLogger(): void {
-        const logger = this.getLogger();
-        logger.level = process.env.LOGLEVEL;
+        // const logger = this.getLogger();
+        // logger.level = process.env.LOGLEVEL;
+        const logger = winston.createLogger({
+            level: process.env.LOGLEVEL,
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            transports: [
+                new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
+                new winston.transports.File({filename: 'logs/combined.log'}),
+            ],
+        });
+
+        if (process.env.NODE_ENV !== 'production') {
+            const consoleFormat = winston.format.printf(({ level, message, timestamp }) => {
+                return `${timestamp} | ${level} » ${message}`;
+            });
+
+            logger.add(new winston.transports.Console({
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.simple(),
+                    consoleFormat
+                ),
+            }));
+        }
+
+        this.logger = logger;
     }
 
-    public getLogger = (): Logger => getLogger();
+    public getLogger = (): winston.Logger => this.logger;
     public getKeycloak = (): Keycloak.Keycloak => this.keycloak;
     public getPrisma = (): PrismaClient => this.prisma;
     public getKeycloakAdmin = (): KeycloakAdmin => this.keycloakAdmin;
