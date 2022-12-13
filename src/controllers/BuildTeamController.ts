0114b3a39a7fc33ee2b8136c2a7d72ca96673dc7
Robin Ferch <robin@ferch-mail.de>
2022-12-13 20:53:50 +0100

    feat(api): Add buildteam list pagination

--- src/controllers/BuildTeamController.ts
+++ src/controllers/BuildTeamController.ts
@@ -10,14 +10,30 @@ class BuildTeamController {
     }
 
     public async getBuildTeams(req: Request, res: Response) {
-        const buildteams = await this.core.getPrisma().buildTeam.findMany({
-            include: {
-                _count: {
-                    select: { members: true, builds: true }
+        if (req.query && req.query.page) {
+            let page = parseInt(req.query.page as string);
+            const buildteams = await this.core.getPrisma().buildTeam.findMany({
+                skip: page * 10,
+                take: 10,
+                include: {
+                    _count: {
+                        select: { members: true, builds: true }
+                    }
                 }
-            }
-        });
-        res.send(buildteams);
+            });
+            let count = await this.core.getPrisma().buildTeam.count();
+            res.send({pages: Math.ceil(count / 10), data: buildteams});
+        } else {
+            const buildteams = await this.core.getPrisma().buildTeam.findMany({
+                include: {
+                    _count: {
+                        select: { members: true, builds: true }
+                    }
+                }
+            });
+            res.send(buildteams);
+        }
+
     }
 }
 
--- src/web/routes/index.ts
+++ src/web/routes/index.ts
@@ -27,7 +27,7 @@ class Routes {
 
         const buildTeamController = new BuildTeamController(this.web.getCore());
 
-        legacyRouter.addRoute(RequestMethods.GET, "/modpack/images", (request, response) => {
+        legacyRouter.addRoute(RequestMethods.GET, "modpack/images", (request, response) => {
             response.send({"1":{"url":"https://i.imgur.com/N5cplwx.jpeg","credit":"waggiswagen#2266, Knäggi#4895 render: jo_kil#1977"},"2":{"url":"https://i.imgur.com/tGtWJGk.jpeg","credit":"Woesh3#1155"},"3":{"url":"https://i.imgur.com/yxEWCdQ.jpeg","credit":"Juancy23#9223"},"4":{"url":"https://i.imgur.com/yQjMRlr.jpeg","credit":"Leander#2813, Grischbrei#6173, Norwod#9035 & DasBirnenDing#1574"},"5":{"url":"https://i.imgur.com/9zqFHxa.png","credit":"Schnieven#0083, XilefHD#7198, copac#6194, render: XilefHD#7198"},"logo":{"url":"https://i.imgur.com/ih6BF72.png","credit":null}})
         });
 
@@ -35,11 +35,10 @@ class Routes {
             response.send({status: "up"});
         });
 
+
         router.addRoute(RequestMethods.GET, '/buildteams', async (request, response) => {
             await buildTeamController.getBuildTeams(request, response);
-        },
-            query('').isString(),
-            );
+        }, query('page').isNumeric().optional());
 
     }
 }
