f118c1c452f43815613bfd0de2456f28bf3b0036
Nudesuppe42 <jufo.theo@gmail.com>
2023-02-15 20:19:26 +0100

    feat(buildteams): :sparkles: Single BuildTeam Route

--- src/controllers/BuildTeamController.ts
+++ src/controllers/BuildTeamController.ts
@@ -1,75 +1,95 @@
 import {Request, Response} from "express";
+
 import Core from "../Core.js";
 import {validationResult} from "express-validator";
 
 class BuildTeamController {
+  private core: Core;
 
-    private core: Core;
+  constructor(core: Core) {
+    this.core = core;
+  }
 
-    constructor(core: Core) {
-        this.core = core;
+  public async getBuildTeams(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    if (req.query && req.query.page) {
+      let page = parseInt(req.query.page as string);
+      const buildteams = await this.core.getPrisma().buildTeam.findMany({
+        skip: page * 10,
+        take: 10,
+        include: {
+          _count: {
+            select: { members: true, builds: true },
+          },
+        },
+      });
+      let count = await this.core.getPrisma().buildTeam.count();
+      res.send({ pages: Math.ceil(count / 10), data: buildteams });
+    } else {
+      const buildteams = await this.core.getPrisma().buildTeam.findMany({
+        include: {
+          _count: {
+            select: { members: true, builds: true },
+          },
+        },
+      });
+      res.send(buildteams);
+    }
+  }
+  public async getBuildTeam(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const buildteams = await this.core.getPrisma().buildTeam.findFirst({
+      where: { id: req.params.id },
+      include: {
+        _count: {
+          select: { members: true, builds: true },
+        },
+      },
+    });
+    res.send(buildteams);
+  }
+
+  public async getBuildTeamApplicationQuestion(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    public async getBuildTeams(req: Request, res: Response) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        if (req.query && req.query.page) {
-            let page = parseInt(req.query.page as string);
-            const buildteams = await this.core.getPrisma().buildTeam.findMany({
-                skip: page * 10,
-                take: 10,
-                include: {
-                    _count: {
-                        select: { members: true, builds: true }
-                    }
-                }
-            });
-            let count = await this.core.getPrisma().buildTeam.count();
-            res.send({pages: Math.ceil(count / 10), data: buildteams});
-        } else {
-            const buildteams = await this.core.getPrisma().buildTeam.findMany({
-                include: {
-                    _count: {
-                        select: { members: true, builds: true }
-                    }
-                }
-            });
-            res.send(buildteams);
-        }
+    let buildteam = await this.core.getPrisma().buildTeam.findUnique({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        applicationQuestions: true,
+      },
+    });
 
+    if (buildteam) {
+      res.send(buildteam.applicationQuestions);
+    } else {
+      res
+        .status(404)
+        .send({
+          code: 404,
+          message: "Buildteam does not exit.",
+          translationKey: "404",
+        });
     }
+  }
 
-    public async getBuildTeamApplicationQuestion(req: Request, res: Response) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        let buildteam = await this.core.getPrisma().buildTeam.findUnique({
-            where: {
-                id: req.params.id
-            },
-            include: {
-                applicationQuestions: true
-            }
-        })
-
-        if(buildteam) {
-            res.send(buildteam.applicationQuestions)
-        } else {
-            res.status(404).send({code: 404, message: "Buildteam does not exit.", translationKey: "404"})
-        }
+  public async updateBuildTeamApplicationQuestion(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-
-    public async updateBuildTeamApplicationQuestion(req: Request, res: Response) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        res.send("test")
-    }
+    res.send("test");
+  }
 }
 
 export default BuildTeamController
--- src/web/routes/index.ts
+++ src/web/routes/index.ts
@@ -1,55 +1,111 @@
-import Web from '../Web.js';
-import Router from './utils/Router.js';
-import {RequestMethods} from './utils/RequestMethods.js';
-import {Keycloak} from "keycloak-connect";
+import { Request, Response, request } from "express";
+import { param, query } from "express-validator";
+
 import BuildTeamController from "../../controllers/BuildTeamController.js";
-import {param, query} from "express-validator";
-import {Request, request, Response} from "express";
+import { Keycloak } from "keycloak-connect";
+import { RequestMethods } from "./utils/RequestMethods.js";
+import Router from "./utils/Router.js";
+import Web from "../Web.js";
 
 class Routes {
-    app;
+  app;
 
-    web: Web;
+  web: Web;
 
-    keycloak: Keycloak;
+  keycloak: Keycloak;
 
-    constructor(web: Web) {
-        web.getCore().getLogger().info('Registering API routes');
-        this.web = web;
-        this.app = web.getApp();
-        this.keycloak = this.web.getKeycloak();
-        this.registerRoutes();
+  constructor(web: Web) {
+    web.getCore().getLogger().info("Registering API routes");
+    this.web = web;
+    this.app = web.getApp();
+    this.keycloak = this.web.getKeycloak();
+    this.registerRoutes();
+  }
 
-    }
+  private registerRoutes() {
+    const legacyRouter: Router = new Router(this.web, "");
+    const router: Router = new Router(this.web, "v1");
 
-    private registerRoutes() {
-        const legacyRouter: Router = new Router(this.web, "");
-        const router: Router = new Router(this.web, "v1");
+    const buildTeamController = new BuildTeamController(this.web.getCore());
 
-        const buildTeamController = new BuildTeamController(this.web.getCore());
-
-        legacyRouter.addRoute(RequestMethods.GET, "modpack/images", (request, response) => {
-            response.send({"1":{"url":"https://i.imgur.com/N5cplwx.jpeg","credit":"waggiswagen#2266, Knäggi#4895 render: jo_kil#1977"},"2":{"url":"https://i.imgur.com/tGtWJGk.jpeg","credit":"Woesh3#1155"},"3":{"url":"https://i.imgur.com/yxEWCdQ.jpeg","credit":"Juancy23#9223"},"4":{"url":"https://i.imgur.com/yQjMRlr.jpeg","credit":"Leander#2813, Grischbrei#6173, Norwod#9035 & DasBirnenDing#1574"},"5":{"url":"https://i.imgur.com/9zqFHxa.png","credit":"Schnieven#0083, XilefHD#7198, copac#6194, render: XilefHD#7198"},"logo":{"url":"https://i.imgur.com/ih6BF72.png","credit":null}})
+    legacyRouter.addRoute(
+      RequestMethods.GET,
+      "modpack/images",
+      (request, response) => {
+        response.send({
+          "1": {
+            url: "https://i.imgur.com/N5cplwx.jpeg",
+            credit: "waggiswagen#2266, Knäggi#4895 render: jo_kil#1977",
+          },
+          "2": {
+            url: "https://i.imgur.com/tGtWJGk.jpeg",
+            credit: "Woesh3#1155",
+          },
+          "3": {
+            url: "https://i.imgur.com/yxEWCdQ.jpeg",
+            credit: "Juancy23#9223",
+          },
+          "4": {
+            url: "https://i.imgur.com/yQjMRlr.jpeg",
+            credit:
+              "Leander#2813, Grischbrei#6173, Norwod#9035 & DasBirnenDing#1574",
+          },
+          "5": {
+            url: "https://i.imgur.com/9zqFHxa.png",
+            credit:
+              "Schnieven#0083, XilefHD#7198, copac#6194, render: XilefHD#7198",
+          },
+          logo: { url: "https://i.imgur.com/ih6BF72.png", credit: null },
         });
+      }
+    );
 
-        router.addRoute(RequestMethods.GET, '/healthcheck', (request, response) => {
-            response.send({status: "up"});
-        });
+    router.addRoute(RequestMethods.GET, "/healthcheck", (request, response) => {
+      response.send({ status: "up" });
+    });
 
+    router.addRoute(
+      RequestMethods.GET,
+      "/buildteams",
+      async (request, response) => {
+        await buildTeamController.getBuildTeams(request, response);
+      },
+      query("page").isNumeric().optional()
+    );
 
-        router.addRoute(RequestMethods.GET, '/buildteams', async (request, response) => {
-            await buildTeamController.getBuildTeams(request, response);
-        }, query('page').isNumeric().optional());
+    router.addRoute(
+      RequestMethods.GET,
+      "/buildteams/:id",
+      async (request, response) => {
+        await buildTeamController.getBuildTeam(request, response);
+      },
+      param("id").isUUID()
+    );
 
-        router.addRoute(RequestMethods.GET, "/buildteams/:id/application/questions", async (request: Request, response: Response) => {
-            await buildTeamController.getBuildTeamApplicationQuestion(request, response)
-        }, param("id").isUUID())
+    router.addRoute(
+      RequestMethods.GET,
+      "/buildteams/:id/application/questions",
+      async (request: Request, response: Response) => {
+        await buildTeamController.getBuildTeamApplicationQuestion(
+          request,
+          response
+        );
+      },
+      param("id").isUUID()
+    );
 
-        router.addRoute(RequestMethods.POST, "/buildteams/:id/application/questions", async (request: Request, response: Response) => {
-            await buildTeamController.getBuildTeamApplicationQuestion(request, response)
-        }, param("id").isUUID())
-
-    }
+    router.addRoute(
+      RequestMethods.POST,
+      "/buildteams/:id/application/questions",
+      async (request: Request, response: Response) => {
+        await buildTeamController.getBuildTeamApplicationQuestion(
+          request,
+          response
+        );
+      },
+      param("id").isUUID()
+    );
+  }
 }
 
 export default Routes;
