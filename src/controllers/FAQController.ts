007ab5e482eadaec369a7c87694ddccd3606145b
Nudesuppe42 <jufo.theo@gmail.com>
2023-03-20 20:06:58 +0100

    feat(faq): ✨ FAQ Questions

---
+++ src/controllers/FAQController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+
+import Core from "../Core.js";
+import { questions } from "../util/QuestionData.js";
+import { validationResult } from "express-validator";
+import yup from "yup";
+
+class FaqController {
+  private core: Core;
+
+  constructor(core: Core) {
+    this.core = core;
+  }
+
+  public async getFaqQuestions(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    if (req.query && req.query.page) {
+      let page = parseInt(req.query.page as string);
+      const questions = await this.core.getPrisma().fAQQuestion.findMany({
+        skip: page * 10,
+        take: 10,
+      });
+      let count = await this.core.getPrisma().fAQQuestion.count();
+      res.send({ pages: Math.ceil(count / 10), data: questions });
+    } else {
+      const questions = await this.core.getPrisma().fAQQuestion.findMany({});
+      res.send(questions);
+    }
+  }
+}
+
+export default FaqController;
