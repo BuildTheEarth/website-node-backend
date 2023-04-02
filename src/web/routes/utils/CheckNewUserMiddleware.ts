de11c3c874f6add47c67a7bc738fdb55b2ad363c
Nudesuppe42 <jufo.theo@gmail.com>
2023-04-02 20:12:58 +0200

    feat(middleware): ✨ Give initial users account.info permissions

--- src/web/routes/utils/CheckNewUserMiddleware.ts
+++ src/web/routes/utils/CheckNewUserMiddleware.ts
@@ -1,78 +1,84 @@
 import {NextFunction, Request, Response} from "express";
-import {PrismaClient} from "@prisma/client";
+
 import Core from "../../../Core.js";
+import { PrismaClient } from "@prisma/client";
 
 const checkNewUser = (prisma: PrismaClient, core: Core) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        const user = await prisma.user.findUnique({
-            where: {
-                ssoId: req.kauth.grant.access_token.content.sub
-            }
-        })
-        if(user) {
-            const kcuser = await core.getKeycloakAdmin().getKeycloakAdminClient().users.findOne({
-                id: req.kauth.grant.access_token.content.sub
-            })
-            if(kcuser.federatedIdentities.length > 0) {
-                const discordIdentity = kcuser.federatedIdentities.find((fi) => fi.identityProvider === "discord")
-                if(discordIdentity) {
-                    if(user.discordId !== discordIdentity.userId) {
-                        const user = await prisma.user.update({
-                            where: {
-                                ssoId: req.kauth.grant.access_token.content.sub
-                            },
-                            data: {
-                                discordId: discordIdentity.userId
-                            }
-                        })
-                    }
-                } else {
-                    const user = await prisma.user.update({
-                        where: {
-                            ssoId: req.kauth.grant.access_token.content.sub
-                        },
-                        data: {
-                            discordId: ""
-                        }
-                    })
-                }
-            } else {
-                const user = await prisma.user.update({
-                    where: {
-                        ssoId: req.kauth.grant.access_token.content.sub
-                    },
-                    data: {
-                        discordId: ""
-                    }
-                })
-            }
-            next();
-            return;
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        ssoId: req.kauth.grant.access_token.content.sub,
+      },
+    });
+    if (user) {
+      const kcuser = await core
+        .getKeycloakAdmin()
+        .getKeycloakAdminClient()
+        .users.findOne({
+          id: req.kauth.grant.access_token.content.sub,
+        });
+      if (kcuser.federatedIdentities.length > 0) {
+        const discordIdentity = kcuser.federatedIdentities.find(
+          (fi) => fi.identityProvider === "discord"
+        );
+        if (discordIdentity) {
+          if (user.discordId !== discordIdentity.userId) {
+            const user = await prisma.user.update({
+              where: {
+                ssoId: req.kauth.grant.access_token.content.sub,
+              },
+              data: {
+                discordId: discordIdentity.userId,
+              },
+            });
+          }
         } else {
-
-            const kcuser = await core.getKeycloakAdmin().getKeycloakAdminClient().users.findOne({
-                id: req.kauth.grant.access_token.content.sub
-            })
-            const discordIdentity = kcuser.federatedIdentities.find((fi) => fi.identityProvider === "discord")
-            if (discordIdentity) {
-                await prisma.user.create({
-                    data: {
-                        ssoId: req.kauth.grant.access_token.content.sub,
-                        discordId: discordIdentity.userId
-                    }
-                })
-            } else {
-                await prisma.user.create({
-                    data: {
-                        ssoId: req.kauth.grant.access_token.content.sub
-                    }
-                })
-            }
-
+          const user = await prisma.user.update({
+            where: {
+              ssoId: req.kauth.grant.access_token.content.sub,
+            },
+            data: {
+              discordId: "",
+            },
+          });
         }
-        next()
+      } else {
+        const user = await prisma.user.update({
+          where: {
+            ssoId: req.kauth.grant.access_token.content.sub,
+          },
+          data: {
+            discordId: "",
+          },
+        });
+      }
+      next();
+      return;
+    } else {
+      const kcuser = await core
+        .getKeycloakAdmin()
+        .getKeycloakAdminClient()
+        .users.findOne({
+          id: req.kauth.grant.access_token.content.sub,
+        });
+      const discordIdentity = kcuser.federatedIdentities.find(
+        (fi) => fi.identityProvider === "discord"
+      );
+      const user = await prisma.user.create({
+        data: {
+          ssoId: req.kauth.grant.access_token.content.sub,
+          discordId: discordIdentity ? discordIdentity.userId : undefined,
+        },
+      });
+      await prisma.userPermission.create({
+        data: {
+          user: { connect: { id: user.id } },
+          permission: "account.info",
+        },
+      });
     }
-
-}
+    next();
+  };
+};
 
 export default checkNewUser;
