7e229b8a770d5e124a1dbedab15609f23579ba83
Nudesuppe42 <jufo.theo@gmail.com>
2023-06-21 20:14:33 +0200

    feat(application): ✨ Applications

--- src/controllers/BuildTeamController.ts
+++ src/controllers/BuildTeamController.ts
@@ -261,6 +261,13 @@ class BuildTeamController {
     });
     res.send(buildteam);
   }
+
+  public async apply(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
 }
 
 export default BuildTeamController
