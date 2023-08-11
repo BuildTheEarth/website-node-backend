5bae450abcb9860829afc4df3f748a56113bff3d
frikandelworst <66461508+frikandelworst@users.noreply.github.com>
2023-08-11 14:44:20 +0200

    feat(newsletters): ✨ Add Newsletter Endpoint
    * feat(newsletters): ✨ Add Newsletter Question
    
    * feat(newsletters): ✨ Formatted code of previous commit
    
    * feat(newsletters): ✨ Formatted code of previous commits again

--- src/controllers/ContactController.ts
+++ src/controllers/ContactController.ts
@@ -1,52 +1,50 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 
 import Core from "../Core.js";
-import { questions } from "../util/QuestionData.js";
-import { validationResult } from "express-validator";
-import yup from "yup";
+import {validationResult} from "express-validator";
 
 class ContactController {
-  private core: Core;
+    private core: Core;
 
-  constructor(core: Core) {
-    this.core = core;
-  }
-
-  public async getContacts(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    constructor(core: Core) {
+        this.core = core;
     }
 
-    const contacts = await this.core.getPrisma().contact.findMany({});
-    res.send(contacts);
-  }
+    public async getContacts(req: Request, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
 
-  public async addContact(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const { name, role, discord, email, avatar } = req.body;
-    const contact = await this.core.getPrisma().contact.create({
-      data: { name, role, discord, email, avatar },
-    });
-    res.send(contact);
-  }
-
-  public async editContact(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+        const contacts = await this.core.getPrisma().contact.findMany({});
+        res.send(contacts);
     }
 
-    const { name, role, discord, email, avatar } = req.body;
-    const contact = await this.core.getPrisma().contact.update({
-      where: { id: req.params.id },
-      data: { name, role, discord, email, avatar },
-    });
-    res.send(contact);
-  }
+    public async addContact(req: Request, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        const {name, role, discord, email, avatar} = req.body;
+        const contact = await this.core.getPrisma().contact.create({
+            data: {name, role, discord, email, avatar},
+        });
+        res.send(contact);
+    }
+
+    public async editContact(req: Request, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {name, role, discord, email, avatar} = req.body;
+        const contact = await this.core.getPrisma().contact.update({
+            where: {id: req.params.id},
+            data: {name, role, discord, email, avatar},
+        });
+        res.send(contact);
+    }
 }
 
 export default ContactController;
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,46 +1,44 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 
 import Core from "../Core.js";
-import { questions } from "../util/QuestionData.js";
-import { validationResult } from "express-validator";
-import yup from "yup";
+import {validationResult} from "express-validator";
 
 class UserController {
-  private core: Core;
+    private core: Core;
 
-  constructor(core: Core) {
-    this.core = core;
-  }
+    constructor(core: Core) {
+        this.core = core;
+    }
 
-  public async getUsers(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    public async getUsers(req: Request, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        if (req.query && req.query.page) {
+            let page = parseInt(req.query.page as string);
+            const users = await this.core.getPrisma().user.findMany({
+                skip: page * 10,
+                take: 10,
+            });
+            let count = await this.core.getPrisma().user.count();
+            res.send({pages: Math.ceil(count / 10), data: users});
+        } else {
+            const users = await this.core.getPrisma().user.findMany({});
+            res.send(users);
+        }
     }
-    if (req.query && req.query.page) {
-      let page = parseInt(req.query.page as string);
-      const users = await this.core.getPrisma().user.findMany({
-        skip: page * 10,
-        take: 10,
-      });
-      let count = await this.core.getPrisma().user.count();
-      res.send({ pages: Math.ceil(count / 10), data: users });
-    } else {
-      const users = await this.core.getPrisma().user.findMany({});
-      res.send(users);
-    }
-  }
 
-  public async getPermissions(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    public async getPermissions(req: Request, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        const permissions = await this.core
+            .getPrisma()
+            .userPermission.findMany({where: {userId: req.params.id}});
+        res.send(permissions);
     }
-    const permissions = await this.core
-      .getPrisma()
-      .userPermission.findMany({ where: { userId: req.params.id } });
-    res.send(permissions);
-  }
 }
 
 export default UserController;
