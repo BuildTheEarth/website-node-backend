793f964da4129b7c8a4158669990e00586e64a82
frikandelworst <kyanvde@gmail.com>
2023-08-11 14:31:46 +0200

    feat(newsletters): ✨ Formatted code of previous commit

--- src/controllers/NewsletterController.ts
+++ src/controllers/NewsletterController.ts
@@ -1,72 +1,79 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 
 import Core from "../Core.js";
-import { questions } from "../util/QuestionData.js";
-import { validationResult } from "express-validator";
+import {questions} from "../util/QuestionData.js";
+import {validationResult} from "express-validator";
 import yup from "yup";
 
 class NewsletterController {
-  private core: Core;
+    private core: Core;
 
-  constructor(core: Core) {
-    this.core = core;
-  }
+    constructor(core: Core) {
+        this.core = core;
+    }
 
-  public async getNewsletters(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    public async getNewsletters(req: Request, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        if (req.query && req.query.page) {
+            let page = parseInt(req.query.page as string);
+            const newsletters = await this.core.getPrisma().newsletter.findMany({
+                skip: page * 10,
+                take: 10,
+            });
+            let count = await this.core.getPrisma().newsletter.count();
+            res.send({pages: Math.ceil(count / 10), data: newsletters});
+        } else {
+            const newsletters = await this.core.getPrisma().newsletter.findMany({});
+            res.send(newsletters);
+        }
     }
-    if (req.query && req.query.page) {
-      let page = parseInt(req.query.page as string);
-      const newsletters = await this.core.getPrisma().newsletter.findMany({
-        skip: page * 10,
-        take: 10,
-      });
-      let count = await this.core.getPrisma().newsletter.count();
-      res.send({ pages: Math.ceil(count / 10), data: newsletters });
-    } else {
-      const newsletters = await this.core.getPrisma().newsletter.findMany({});
-      res.send(newsletters);
-    }
-  }
-  public async getNewsletter(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const newsletter = await this.core.getPrisma().newsletter.findUnique({
-      where: req.query.isIssue
-        ? {
-            issue: parseInt(req.params.id as string),
-          }
-        : {
-            id: req.params.id,
-          },
-    });
-    if (newsletter) {
-      res.send(newsletter);
-    } else {
-      res.status(404).send({
-        code: 404,
-        message: "Newsletter does not exist.",
-        translationKey: "404",
-      });
-    }
-    return;
-  }
 
-  public async addNewsletter(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array() });
+    public async getNewsletter(req: Request, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        const newsletter = await this.core.getPrisma().newsletter.findUnique({
+            where: req.query.isIssue
+                ? {
+                    issue: parseInt(req.params.id as string),
+                }
+                : {
+                    id: req.params.id,
+                },
+        });
+        if (newsletter) {
+            res.send(newsletter);
+        } else {
+            res.status(404).send({
+                code: 404,
+                message: "Newsletter does not exist.",
+                translationKey: "404",
+            });
+        }
+        return;
     }
-    const issue = await this.core.getPrisma().newsletter.count() + 1;
-    const newsletter = await this.core.getPrisma().newsletter.create({
-      data: {issue: issue,title: req.body.title, published_date: new Date(), links: req.body.links, public: req.body.public ? req.body.public : true},
-    });
-    res.send(newsletter);
-  }
+
+    public async addNewsletter(req: Request, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        const issue = await this.core.getPrisma().newsletter.count() + 1;
+        const newsletter = await this.core.getPrisma().newsletter.create({
+            data: {
+                issue: issue,
+                title: req.body.title,
+                published_date: new Date(),
+                links: req.body.links,
+                public: req.body.public ? req.body.public : true
+            },
+        });
+        res.send(newsletter);
+    }
 }
 
 export default NewsletterController;
