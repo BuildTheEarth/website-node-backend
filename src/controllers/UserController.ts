19b5faf45c74d543402ab58392581e12da071552
Nudesuppe42 <67996941+Nudelsuppe42@users.noreply.github.com>
2023-12-23 17:02:33 +0100

    fix(src): Various Bugfixes (#27)
    * feat(auth): :ambulance: TEMP: Migrate Accounts without a valid SSO ID
    
    * feat(auth): :ambulance: TEMP: Migrate Users without valid SSO ID
    
    * chore(auth): :mute: Remove console logs
    
    * fix(users): :bug: remove duplicates from created Buildteams list
    
    * fix(application): :bug: Crash on global permissions fix

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -92,6 +92,7 @@ class UserController {
             userId: user.id,
           },
         },
+        id: { notIn: user.createdBuildTeams.map((b) => b.id) },
       },
       select: {
         id: true,
@@ -179,7 +180,11 @@ class UserController {
     const reviewPermissions = await this.core
       .getPrisma()
       .userPermission.findMany({
-        where: { userId: user.id, permissionId: "team.application.review" },
+        where: {
+          userId: user.id,
+          permissionId: "team.application.review",
+          buildTeamId: { not: null },
+        },
         select: { buildTeamId: true, id: true },
       });
 
